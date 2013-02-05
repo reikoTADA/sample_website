f8f176f4c8bf210e0a8ebf3ab46b95e95d227d83
reikoTADA <aug21reiko@yahoo.co.jp>
2013-02-05 16:09:29 +0900

    [#43826259] テストを追加
    以下の検証シナリオを追加
    - about ページ
    - contacts ページ

--- features/step_definitions/sample_website_steps.rb
+++ features/step_definitions/sample_website_steps.rb
@@ -3,6 +3,10 @@
 もし /^"(.*?)"ページを表示する$/ do |page_name|
   if page_name == "トップ"
     path = "/"
+  elsif page_name == "about"
+    path = "/about"
+  elsif page_name == "お問い合わせ"
+    path = "/contacts"
   end
   visit path
 end
